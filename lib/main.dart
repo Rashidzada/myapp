56f5cbeab85754648610f3245aeaa2f1b8727a0f
Rashidzada <rashidzada6@gmail.com>
2023-11-22 08:33:26 +0500

    first time to push

--- lib/main.dart
+++ lib/main.dart
@@ -124,4 +124,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
+// }
+// git config --global user.email "you@example.com"
+//   git config --global user.name "Your Name"
